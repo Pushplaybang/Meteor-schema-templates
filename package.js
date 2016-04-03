29bdb39472889eab021fdab0f6da816f53562e0d
Paul van Zyl <paul@nonacreative.com>
2016-04-03 15:26:41 +0200

    adding a version for collection 2 depemdancy
    a

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.use('aldeed:collection2');
+  api.use('aldeed:collection2@2.9.0');
   api.addFiles('_namespace.js');
   api.addFiles('templates/dates.js');
   api.addFiles('templates/user.js');
